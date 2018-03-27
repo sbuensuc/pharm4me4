c24bfe16e6c7292c31a77046c1dba7618162af1f
Sam <Sam@DESKTOP-O0S9A91>
2018-03-27 16:52:47 -0400

    Add project files.

---
+++ pharm4me4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pharm4me4.MvcApplication" Language="C#" %>
---
+++ pharm4me4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pharm4me4.Startup))]
+namespace pharm4me4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
