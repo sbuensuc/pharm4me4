32a241f150ece7ee25446df5ec7e38f5164c7bbb
Sam <Sam@Home>
2018-03-27 18:53:18 -0400

    ...

--- pharm4me4/App_Start/BundleConfig.cs
+++ pharm4me4/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace pharm4me4
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
